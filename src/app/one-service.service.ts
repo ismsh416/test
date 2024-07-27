6fdf788b68cbe02556c3aac440535aa0b14c55f9
ismail <ismailsh416@gmail.com>
2024-07-28 00:20:35 +0530

    code

--- src/app/one-service.service.ts
+++ src/app/one-service.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { One } from './one.model';
 
@@ -8,9 +8,30 @@ import { One } from './one.model';
 })
 export class OneServiceService {
   private apiUrl = 'http://localhost:8080/one';
+  private create = 'http://localhost:8080/one/create';
+
+  headers = new HttpHeaders()
+            .set('Content-Type', 'application/json')
+            .set('Accept', 'application/json');
+ 
+  httpOptions = {
+    headers: this.headers
+  };
 
   constructor(private http: HttpClient) { }
   getOnes():Observable<One[]> {
     return this.http.get<One[]>(this.apiUrl);
+  }
+  addOne(one: {name :string}): Observable<One> {
+    return this.http.post<One>(this.create, one);
+  }
+  deleteOne(id: number):Observable<void> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.delete<void>(url);
+  }
+  findByItem(id: number): Observable<One> {
+    const url = `${this.apiUrl}/${id}`;
+    console.log(url);
+    return this.http.get<One>(url);
   }
 }
--- src/app/onelist/onelist.component.ts
+++ src/app/onelist/onelist.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { One } from '../one.model';
 import { OneServiceService } from '../one-service.service';
 import { CommonModule, NgFor, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-onelist',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './onelist.component.html',
   styleUrl: './onelist.component.css'
 })
@@ -14,9 +15,14 @@ export class OnelistComponent implements OnInit{
   onelist: One[] =[];
   loading =true;
   error:string|null= null;
-  constructor(private oenService: OneServiceService ) {}
+  newItem: {name:string} = {name:''};
+  constructor(private oneService: OneServiceService ) {}
   ngOnInit(): void {
-    this.oenService.getOnes().subscribe(
+    this.loadItems();
+  }
+
+  loadItems(): void{
+    this.oneService.getOnes().subscribe(
       (data: One[]) => {
         this.onelist = data;
         this.loading = false;
@@ -26,7 +32,36 @@ export class OnelistComponent implements OnInit{
         this.loading = false;
       }
     );
-    
+  }
+  addItem(): void {
+    if (!this.newItem.name) {
+      return; // Ensure all fields are filled
+    }
+
+    console.log('------')
+    this.oneService.addOne(this.newItem).subscribe(
+      (data: One) => {
+        this.onelist.push(data); // Add the new item to the list
+        this.newItem = { name: '' }; // Reset form
+      },
+      (error) => {
+        this.error = 'Failed to add item'+ error.message
+            }
+    );
   }
 
+  removeItem(id: number): void {
+    this.oneService.deleteOne(id).subscribe(
+      () => {
+        this.onelist = this.onelist.filter(item => item.id !== id); // Remove item from list
+      },
+      (error) => {
+        this.error = 'Failed to remove item';
+      }
+    );
+  
+  }
+  findByItem(id: number): void{
+    this.oneService.findByItem(id)
+  }
 }
